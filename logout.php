aad51d19bc3b084d832a408fe0f00f7e443d4e9b
eurisj <eurisjesus32@gmail.com>
2025-04-13 19:53:14 -0400

    Update logout.php

--- logout.php
+++ logout.php
@@ -2,6 +2,6 @@
 session_start();          // Inicia la sesión
 session_unset();          // Limpia las variables de sesión
 session_destroy();        // Destruye la sesión
-header("Location: INDE.php"); // Redirige a la página principal
+header("Location: index.php"); // Redirige a la página principal
 exit();
 ?>
